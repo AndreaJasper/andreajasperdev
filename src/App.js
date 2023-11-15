c36738ff4060723edd6280546ca58639a4189787
jasper_a <andrea.nicole.jasper@gmail.com>
2023-11-15 15:15:44 -0700

    adds project page route

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         {/* <Route path="/contact" element={<Contact />} /> */}
-        {/* <Route path="/projects" element={<Projects />} /> */}
+        <Route path="/projects" element={<Projects />} />
       </Routes>
       {/* <Footer /> */}
     </Router>
