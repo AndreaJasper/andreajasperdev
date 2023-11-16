7c4127a8bd917a2a601abe8974ba947e8e61b64a
jasper_a <andrea.nicole.jasper@gmail.com>
2023-11-16 16:37:02 -0700

    adds services to homepage

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
-import { techStackDetails, personalDetails } from "../Details";
+import { services } from "../Services";
+import { personalDetails } from "../Details";
 
 export default function Home() {
-    const { about, values, other } = personalDetails;
+    const { values, other } = personalDetails;
 
     return (
         <>
@@ -29,42 +30,40 @@ export default function Home() {
             </div>
             <div className="pt-24 sm:pt-32">
                 <div className="py-24 sm:py-32 bg-zinc-200">
-                    <div className="mx-auto max-w-7xl px-6 lg:px-8 sm:text-center">
-                        <h2 className="mt-2 mb-16 text-3xl font-bold tracking-tight text-center text-gray-900 sm:text-4xl">testimonials</h2>
+                    <div className="mx-auto max-w-7xl px-6 lg:px-8 text-center">
+                        <h2 className="mt-2 mb-16 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">testimonials</h2>
                         <p className="mt-6 text-lg leading-8 text-gray-600">
                             Coworker testimonials here.
                         </p>
                     </div>
                 </div>
             </div>
-            <div className="py-24 sm:py-32">
+            <div className="pt-24 sm:pt-32">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
+                    <h2 className="mt-2 mb-16 text-3xl font-bold tracking-tight text-gray-900 text-center sm:text-4xl">My Services</h2>
                     <div className="grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-                        <img
-                            src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
-                            alt="Product screenshot"
-                            className="w-[48rem] max-w-xl rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
-                            width={2432}
-                            height={1442}
-                        />
-                        <div className="lg:pl-16 lg:pt-4">
-                            <div className="lg:max-w-lg">
-                                <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Content here</h2>
-                                <p className="mt-6 text-lg leading-8 text-gray-600">
-                                    {other}
-                                </p>
-                            </div>
-                        </div>
+                        {React.Children.toArray(
+                            services.map(({ title, description }) => (
+                                <div className="border border-gray-600 rounded-xl lg:max-w-xl">
+                                    <div className="p-8">
+                                        <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">{title}</h2>
+                                        <p className="mt-6 text-lg leading-8 text-gray-600">
+                                            {description}
+                                        </p>
+                                    </div>
+                                </div>
+                            ))
+                        )}
                     </div>
                 </div>
             </div>
-            <div className="pt-24 sm:pt-32">
+            <div className="py-24 sm:pt-32">
                 <div className="mx-auto max-w-7xl px-6 lg:px-8">
                     <div className="grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
                         <div className="lg:max-w-lg">
                             <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Something else here</h2>
                             <p className="mt-6 text-lg leading-8 text-gray-600">
-                                {values}
+                                {other}
                             </p>
                         </div>
                         <div className="lg:pl-16 lg:pt-4">
---
+++ src/Services.js
@@ -0,0 +1,21 @@
+// Enter your Project Details here
+export const services = [
+    {
+        title: "Software Engineer",
+        // image: hapynOne,
+        description: `Hapyn is an emerging social media platform focused on facilitating connections for professional networking. I implemented an innovative rich text feature that empowers users to craft more comprehensive, visually appealing, and engaging posts.`,
+        summary: `Hapyn is an emerging social media platform focused on facilitating connections for professional networking. I implemented an innovative rich text feature that empowers users to craft more comprehensive, visually appealing, and engaging posts.`,
+        techstack: "React on Rails, HTML/CSS, JavaScript, CKEditor",
+        previewLink: "https://hapyn.com/",
+        githubLink: "",
+    },
+    {
+        title: "Project Manager",
+        // image: hapynTwo,
+        description: `Hapyn is a growing social media platform with a primary focus on enhancing professional networking opportunities. I revamped their resource section to enable users to create any resource from a single page, streamlining the previous multi-step process.`,
+        summary: `Hapyn is a growing social media platform with a primary focus on enhancing professional networking opportunities. I revamped their resource section to enable users to create any resource from a single page, streamlining the previous multi-step process.`,
+        techstack: "React on Rails, HTML/CSS, JavaScript",
+        previewLink: "https://hapyn.com/",
+        githubLink: "",
+    },
+];
